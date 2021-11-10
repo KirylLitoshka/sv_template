4395ce651633804ae4551f2dc9402da722996704
Kirill Litoshko <kovzik08@gmail.com>
2021-11-10 16:34:50 +0300

    moved files to includes folder

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="footer_text">
+				<span class="Copyright"><strong>Copyright © 2011 - 2021 КЖУП «СВЕТОЧЬ» All rights reserved</strong></span>
+		</div>
+	</div>
+</body>
+</html>
